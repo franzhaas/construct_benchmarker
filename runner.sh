d1f3b08267fa84df8ae5b48b32dfc0f6dbc11ff2
Franz Haas <franz.dominik.haas@gmail.com>
2024-10-07 21:27:58 +0200

    - one more branch...

--- runner.sh
+++ runner.sh
@@ -13,6 +13,7 @@ git rebase remotes/origin/compileStringEncoded3
 git rebase remotes/origin/compileRestreamed
 git rebase remotes/origin/compileChecksum
 git rebase remotes/origin/CompileNullStripped
+git rebase remotes/origin/compilePointerMoreInlining
 uv run py.test tests --benchmark-only --benchmark-save=merged
 uv run py.test-benchmark  compare --csv=merged.csv
 uv run python ../analyser.py
